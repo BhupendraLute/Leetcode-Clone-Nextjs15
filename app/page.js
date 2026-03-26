ebbf2dafc036b964151f9aa169a4925154ff687d
Bhupendra Lute <bhupendralute1234@gmail.com.com>
2026-03-27 00:38:32 +0530

    user stored to database

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+import { onBoardUser } from "@/modules/auth/actions";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  await onBoardUser();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-24">
       <UserButton />
